2e373b1726a633ecb57c300ea48af81c9a090111
mark07x <xiexingjian2003@gmail.com>
2019-08-14 02:25:36 +0800

    Fix Mark as Read Button and Fix Render on Dropdown

--- db/query.ts
+++ db/query.ts
@@ -36,6 +36,7 @@ export default {
 
     //Message
     addMessage: 'INSERT INTO message(`from`, `to`, `content`) values(?, ?, ?)',
+    getMyUnreadMessage: 'SELECT message.* FROM `message` LEFT OUTER JOIN `read` ON message.id=`read`.message AND read.user=? WHERE (`to`=? OR `to` IS NULL) AND (read.user IS NULL) AND (message.id=?)',
     queryMyMessageWithLimit: 'SELECT message.*, user.name AS from_name, `read`.message AS `read` FROM `message` LEFT OUTER JOIN user ON user.id=message.`from` LEFT OUTER JOIN `read` ON message.id=`read`.message AND read.user=? WHERE `to`=? OR `to` IS NULL ORDER BY (`read` IS NOT NULL), id DESC LIMIT ?,?',
     countQueryMyMessageWithLimit: 'SELECT COUNT(*) FROM `message` LEFT OUTER JOIN user ON user.id=message.`from` LEFT OUTER JOIN `read` ON message.id=`read`.message AND read.user=? WHERE `to`=? OR `to` IS NULL',
     queryMyUnreadMessage: 'SELECT message.* FROM `message` LEFT OUTER JOIN `read` ON message.id=`read`.message AND read.user=? WHERE (`to`=? OR `to` IS NULL) AND (read.user IS NULL)',
--- routes/message/mark_as_read.ts
+++ routes/message/mark_as_read.ts
@@ -10,20 +10,34 @@ export default (db : (sql : string, values : any) => Promise<any[]>) => {
             next(createError(401, 'Unauthorized'));
             return;
         }
-        if (isNaN(Number(req.body.messageID))) {
-            next(createError(400, 'Message ID Should Be A Number'));
-            return;
-        }
-        const rs = await db(query.queryMyUnreadMessage, [req.session.userID, req.session.userID]);
-        if (!rs[0]) {
-            db(query.log, [req.session.userID, "Message", Number(req.body.messageID), "Read", false, "Error: Message Not Found"]);
-            next(createError(404, 'Message Not Found'));
-            return;
-        }
+        if (req.body.messageID) {
+            if (isNaN(Number(req.body.messageID))) {
+                next(createError(400, 'Message ID Should Be A Number'));
+                return;
+            }
+            const rs = await db(query.getMyUnreadMessage, [req.session.userID, req.session.userID, Number(req.body.messageID)]);
+            if (!rs[0]) {
+                db(query.log, [req.session.userID, "Message", rs[0].id, "Read", false, "Error: Message Not Found"]);
+                next(createError(404, 'Message Not Found'));
+                return;
+            }
 
-        db(query.log, [req.session.userID, "Message", req.body.messageID, "Read", true, null]);
-        db(query.readMessage, [req.session.userID, req.body.messageID]);
-        res.sendStatus(200);
+            db(query.log, [req.session.userID, "Message", rs[0].id, "Read", true, null]);
+            await db(query.readMessage, [req.session.userID, rs[0].id]);
+            res.sendStatus(200);
+        } else {
+            const rs = await db(query.queryMyUnreadMessage, [req.session.userID, req.session.userID]);
+            if (!rs.length) {
+                db(query.log, [req.session.userID, "Message", null, "Read All", false, "Error: No Message"]);
+                next(createError(404, 'Message Not Found'));
+                return;
+            }
+
+            db(query.log, [req.session.userID, "Message", null, "Read All", true, null]);
+            for (let v of rs)
+                await db(query.readMessage, [req.session.userID, v.id]);
+            res.sendStatus(200);
+        }
     });
     return router;
 };
