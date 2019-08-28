ea9eb1897caa05845796ffc66da573ce47b82c95
mark07x <xiexingjian2003@gmail.com>
2019-08-28 15:18:51 +0800

    Reset db.ts

---
+++ src/db/db.ts
@@ -0,0 +1,24 @@
+import mysql from 'mysql';
+import query from "./query";
+import {AllHtmlEntities} from "html-entities";
+
+const config = require('./DBConfig');
+const pool = mysql.createPool(config);
+
+export default (sql : string, values : string[]) =>
+    new Promise<any>((resolve, reject) => {
+        pool.getConnection(function(err, connection) {
+            if (err) {
+                reject(err);
+            } else {
+                connection.query(sql, values, (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(rows);
+                    }
+                    connection.release();
+                })
+            }
+        })
+    });
